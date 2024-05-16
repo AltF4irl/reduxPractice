9756f5d89b2f61f1fb1833df4dbbc6295975952a
ghostjaby assil <altf4irl@gmail.com>
2024-05-16 13:42:50 +0300

    A butchered version of redux

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Practicing Redux on a small counter miniApp | FullStackOpen Course
+
+Still using the deprecated createStore, this will be taken care of next.
+
+I am here -> A note about the use of createStore
--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  const minusHandler = () => {
-    setCount(count - 1)
-  }
-
-  const plusHandler = () => {
-    setCount(count + 1)
-  }
-
-  const zeroHandler = () => {
-    setCount(0)
-  }
+const App = () => {
 
   return (
     <>
-      <div>
-        <p>{count}</p>
-        <div>
-          <button onClick={minusHandler}>Minus</button>
-          <button onClick={zeroHandler}>Zero</button>
-          <button onClick={plusHandler}>Plus</button>
-        </div>
-      </div>
     </>
   )
 }
