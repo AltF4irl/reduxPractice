ad25945e13a516391aeee27326edd26b70d86f73
ghostjaby assil <altf4irl@gmail.com>
2024-05-17 14:14:41 +0300

    Applying redux states on a mini note app

--- src/main.jsx
+++ src/main.jsx
@@ -1,55 +1,58 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './App.css'
 import './index.css'
 
-import { createStore } from 'redux'
+import App from './App'
 
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default:
-      return state
-  }
-}
+// import { createStore } from 'redux'
 
-const store = createStore(counterReducer)
+// const counterReducer = (state = 0, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1
+//     case 'DECREMENT':
+//       return state - 1
+//     case 'ZERO':
+//       return 0
+//     default:
+//       return state
+//   }
+// }
 
-const App = () => {
-  return (
-    <div>
-      <div>
-        {store.getState()}
-      </div>
-      <button 
-        onClick={() => store.dispatch({ type: 'INCREMENT' })}
-      >
-        plus
-      </button>
-      <button
-        onClick={() => store.dispatch({ type: 'DECREMENT' })}
-      >
-        minus
-      </button>
-      <button 
-        onClick={() => store.dispatch({ type: 'ZERO' })}
-      >
-        zero
-      </button>
-    </div>
-  )
-}
+// const store = createStore(counterReducer)
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+// const App = () => {
+//   return (
+//     <div>
+//       <div>
+//         {store.getState()}
+//       </div>
+//       <button 
+//         onClick={() => store.dispatch({ type: 'INCREMENT' })}
+//       >
+//         plus
+//       </button>
+//       <button
+//         onClick={() => store.dispatch({ type: 'DECREMENT' })}
+//       >
+//         minus
+//       </button>
+//       <button 
+//         onClick={() => store.dispatch({ type: 'ZERO' })}
+//       >
+//         zero
+//       </button>
+//     </div>
+//   )
+// }
 
-const renderApp = () => {
-  root.render(<App />)
-}
+// const root = ReactDOM.createRoot(document.getElementById('root'))
 
-renderApp()
-store.subscribe(renderApp)
+// const renderApp = () => {
+//   root.render(<App />)
+// }
+
+// renderApp()
+// store.subscribe(renderApp)
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
