252f7b947e9350a1cd5c226045682fbfccdb6176
ghostjaby assil <altf4irl@gmail.com>
2024-05-22 14:15:13 +0300

    Added second satate and combined them using combineReducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import NoteForm from './components/NoteForm'
 import Notes from './components/Notes'
 import './App.css'
+import VisibilityFilter from './components/VisibilityFilter'
+
 
 const App = () => {
+
   return (
     <div>
       <NoteForm />
+      <VisibilityFilter />
       <Notes />
     </div>
   )
