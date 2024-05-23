4a1d852bbd17847a8a413a4d52e5f0efdceca8c0
ghostjaby assil <altf4irl@gmail.com>
2024-05-23 13:42:08 +0300

    Chnaged the depricated create store to configure store

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,20 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
 import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-})
 
-const store = createStore(reducer)
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
+})
 
 console.log(store.getState())
 
--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -1,17 +1,18 @@
-const filterReducer = (state = 'ALL', action) => {
-    switch (action.type) {
-      case 'SET_FILTER':
-        return action.payload
-      default:
-        return state
-    }
-}
+import { createSlice } from "@reduxjs/toolkit"
 
-export const filterChange = filter => {
-    return {
-      type: 'SET_FILTER',
-      payload: filter,
+const initialState = 'ALL'
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    filterChange(state, action) {
+      state = action.payload
+      return state
     }
-}
+  }
+})
+
+export const { filterChange } = filterSlice.actions
   
-export default filterReducer
+export default filterSlice.reducer
