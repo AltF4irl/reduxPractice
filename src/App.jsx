79f3fcd2139b7a37047742f760e6ce3d2f98fdb6
ghostjaby assil <altf4irl@gmail.com>
2024-05-25 13:15:48 +0300

    linked app to local backend using json-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import './App.css'
+import { useEffect } from 'react'
 import NoteForm from './components/NoteForm'
 import Notes from './components/Notes'
-import './App.css'
 import VisibilityFilter from './components/VisibilityFilter'
+import noteService from './services/notes'
+import { setNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
 
 
 const App = () => {
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    noteService.getAll().then(notes => dispatch(setNotes(notes)))
+  },[])
 
   return (
     <div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
-
 import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
@@ -16,8 +15,6 @@ const store = configureStore({
     filter: filterReducer
   }
 })
-
-console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
